1bb7955e34bbfd870498e9f4951bcbccf93d15ae
chiang67 <sean.chiang@mail.utoronto.ca>
2020-08-16 15:58:40 -0400

    Comment combination package

--- src/main/kotlin/combination/CombinationGenerator.kt
+++ src/main/kotlin/combination/CombinationGenerator.kt
@@ -1,34 +1,59 @@
 package combination
 
-fun <T> getAllCombinations(set: List<T>, sampleSize: Int): List<List<T>> {
-    val allCombinations: MutableList<MutableList<T>> = ArrayList(choose(set.size, sampleSize))
-    getAllCombinations(set, sampleSize, ArrayList(), 0, allCombinations)
+/**
+ * Returns a list of all combinations that can be obtained of size [sampleSize] from [elements].
+ */
+fun <T> getAllCombinations(elements: List<T>, sampleSize: Int): List<List<T>> {
+    // Precalculate list size
+    val allCombinations: MutableList<MutableList<T>> = ArrayList(choose(elements.size, sampleSize))
+    getAllCombinations(elements, sampleSize, ArrayList(), 0, allCombinations)
     return allCombinations
 }
 
+/**
+ * Returns all combinations of size [sampleSize] from [elements] starting at the element represented by [index].
+ * [curSelect] is the current list of selected elements for the combination, while [allCombinations] is a list
+ * of all the [sampleSize] combinations created so far.
+ */
 private fun <T> getAllCombinations(
-    set: List<T>,
+    elements: List<T>,
     sampleSize: Int,
     curSelect: MutableList<T>,
     index: Int,
     allCombinations: MutableList<MutableList<T>>
 ) {
 
-    if (sampleSize == 0) allCombinations += ArrayList(curSelect)
+    // Note that if index == [elements].size and sampleSize > 0, then [curSelect] is discarded
+    // Hence only combinations of the top level [sampleSize] will be added to [allCombinations]
+
+    // Base Case: No more elements to choose
+    if (sampleSize == 0) {
+        allCombinations.add(ArrayList(curSelect))
+    }
+
+    // Recursive Case: Need to choose more elements
     else {
-        for (i in index until set.size) {
-            curSelect += set[i]
-            getAllCombinations(set, sampleSize - 1, curSelect, i + 1, allCombinations)
-            curSelect -= set[i]
+        // Add each element to the current selection, and increment the index
+        // for the recursive call to avoid adding it again.
+        for (i in index until elements.size) {
+            curSelect += elements[i]
+            getAllCombinations(elements, sampleSize - 1, curSelect, i + 1, allCombinations)
+            curSelect -= elements[i]
         }
     }
 
 }
 
+/**
+ * Returns [setSize]C[sampleSize] in combinatorial notation.
+ */
 fun choose(setSize: Int, sampleSize: Int): Int {
     return factorial(setSize) / (factorial(setSize - sampleSize) * factorial(sampleSize))
 }
 
+/**
+ * Returns [n]!.
+ */
 private fun factorial(n: Int): Int {
     var product = 1
     for (i in n downTo 1) {
