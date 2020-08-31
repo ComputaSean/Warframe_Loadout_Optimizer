9e60826026f2de2d96cc3decfa97f5e380054338
chiang67 <sean.chiang@mail.utoronto.ca>
2020-08-31 11:31:22 -0400

    Replace map with list in ModManager for faster iteration

--- src/main/kotlin/genericGame/modManager/ModManager.kt
+++ src/main/kotlin/genericGame/modManager/ModManager.kt
@@ -4,23 +4,46 @@ import genericGame.modCreator.ModCreatorI
 
 class ModManager {
 
-    private val modCreators = HashSet<ModCreatorI>()
+    private val modCreators = ArrayList<ModCreatorI>()
 
-    fun addModCreator(modCreator: ModCreatorI) {
-        if (modCreators.contains(modCreator))
-            throw ModManagerException("ModCreator already exists in the manager.")
+    fun addModCreator(modCreator: ModCreatorI): Boolean {
+        if (modCreators.contains(modCreator)) return false
         modCreators.add(modCreator)
+        return true
     }
 
-    fun removeModCreator(modCreator: ModCreatorI) {
-        if (!modCreators.remove(modCreator))
-            throw ModManagerException("ModCreator doesn't exist in the manager.")
+    fun getModCreator(index: Int): ModCreatorI? {
+        return if (index >= 0 && index < modCreators.size)
+            modCreators[index]
+        else {
+            null
+        }
     }
 
-    fun clearAll(modCreator: ModCreatorI) = modCreators.clear()
+    fun removeModCreator(index: Int): ModCreatorI? {
+        return if (index >= 0 && index < modCreators.size) {
+            modCreators.removeAt(index)
+        } else {
+            null
+        }
+    }
+
+    fun clearAll() = modCreators.clear()
 
     fun getAll(): List<ModCreatorI> {
         return ArrayList(modCreators)
+    }
+
+    fun size(): Int {
+        return modCreators.size
+    }
+
+    override fun toString(): String {
+        var out = if (modCreators.size > 0) "" else "None\n"
+        for (i in modCreators.indices) {
+            out += String.format("%d. %s\n", i + 1, modCreators[i].name)
+        }
+        return out
     }
 
 }
